be8dcffd40afba0086cf1381bcf5fc1b7686de88
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2024-08-13 15:53:22 +0200

    and then put it back

---
+++ src/routes/elever/[feidenavnPrefix]/dokumenter/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { page } from "$app/stores";
+    /** @type {import('./$types').PageData} */
+    export let data
+</script>
+
+
+<h3>Dette er dokumentene til eleven</h3>
+<p>Elevnummer: {$page.params.dokumentid}</p>
+Her er dokumentet til eleveineenenene
+Hallo
+{JSON.stringify(data)}
