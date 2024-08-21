ce509cb0d95d78ae6a39e5e86c6a003929fe73f0
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2024-08-21 13:59:16 +0200

    logger is nice

--- src/routes/api/students/[feidenavnPrefix]/documents/+server.js
+++ src/routes/api/students/[feidenavnPrefix]/documents/+server.js
@@ -2,6 +2,7 @@ import { env } from '$env/dynamic/private'
 import { getAuthenticatedUser } from '$lib/authentication'
 import { getStudentDocuments } from '$lib/minelev-api/get-student-documents'
 import { json } from '@sveltejs/kit'
+import { logger } from '@vtfk/logger'
 
 export const GET = async ({ params, request, url }) => {
   try {
@@ -10,6 +11,7 @@ export const GET = async ({ params, request, url }) => {
     const documents = await getStudentDocuments(user, studentFeidenavn)
     return json(documents)
   } catch (error) {
+    logger('error', ['Failed when fetching documents', error.response?.data || error.stack || error.toString()])
     return json({ message: 'Failed when fetching documents', error: error.response?.data || error.stack || error.toString() }, { status: 500 })
   }
 }
