e140048949ee5f2d4d4a95534faf315b2b688d3a
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2024-08-22 13:17:54 +0200

    classpage

---
+++ src/routes/klasser/[klasseid]/+layout.svelte
@@ -0,0 +1,64 @@
+<script>
+    import { page } from "$app/stores";
+    /** @type {import('./$types').PageData} */
+    export let data
+
+    let currentClass = data.classes.find(group => group.systemId === $page.params.klasseid)
+  
+    let student = data.students.find(stud => stud.feidenavnPrefix === $page.params.feidenavnPrefix)
+  </script>
+  
+  {#if !currentClass}
+    Du har ikke tilgang på denne klassen
+  {:else}
+    <div class="classBox">
+      <div class="classIcon">
+        <span class="material-symbols-outlined">group</span>
+      </div>
+      <div class="classInfo">
+        <h1 class="classTitle">{currentClass.navn}</h1>
+        <div class="classes">
+          {currentClass.skole}
+        </div>
+      </div>
+    </div>
+    <slot></slot>
+  {/if}
+  
+  <style>
+    .classBox {
+      display: flex;
+      align-items: center;
+      gap: 1.5rem;
+    }
+    .classIcon {
+      width: 4rem;
+      height: 4rem;
+      padding: 1rem;
+      border-radius: 100%;
+      background-color: var(--primary-color-40);
+    }
+    .classIcon span {
+      font-size: 4rem;
+    }
+    .classTitle {
+      padding: 0rem;
+      margin: 0rem;
+    }
+    .classes {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 0.5rem;
+    }
+  
+    @media only screen and (max-width: 768px) {
+      .classIcon {
+        width: 2rem;
+        height: 2rem;
+        padding: 0.5rem;
+      }
+      .classIcon span {
+        font-size: 2rem;
+      }
+    }
+  </style>
--- src/routes/klasser/[klasseid]/+page.svelte
+++ src/routes/klasser/[klasseid]/+page.svelte
@@ -1,7 +1,141 @@
 <script>
-    import { page } from "$app/stores";
+	import { clickOutside } from '$lib/helpers/click-outside'
+	import { goto } from '$app/navigation'
+  import Pagination from '$lib/components/Pagination.svelte';
+  import { page } from "$app/stores";
+	
+
+	/** @type {import('./$types').PageData} */
+	export let data
+
+  let currentClass = data.classes.find(group => group.systemId === $page.params.klasseid)
+
+	let studentsPerPage = 10
+	let currentPage = 0 // zero-indexed
+	let students = data.students.filter(student => student.klasser.some(group => group.systemId === currentClass.systemId))
+	let studentMenus = {}
+	students.forEach(student => {
+		studentMenus[student.elevnummer] = false
+	})
+	let originalStudents = JSON.parse(JSON.stringify(students))
+	let searchValue
+
+	const nextPage = () => {
+		currentPage++
+	}
+	const previousPage = () => {
+		currentPage--
+	}
+	const gotoPage = (pageNumber) => {
+		currentPage = pageNumber
+	}
+
+	const search = (searchValue) => {
+		const filterFunc = (student) => {
+			const sv = searchValue.toLowerCase()
+			return (student.navn.toLowerCase().startsWith(sv) || student.etternavn.toLowerCase().startsWith(sv) || student.klasser.some(group => group.navn.toLowerCase().includes(sv)))
+		}
+		students = originalStudents.filter(filterFunc)
+		currentPage = 0
+	}
 </script>
 
+<h1>Elever i klassen</h1>
+<div class="icon-input" style="width: 16rem;">
+	<span class="material-symbols-outlined">search</span>
+	<input type="text" bind:value={searchValue} on:input={() => { search(searchValue) }} placeholder="Søk etter elev eller klasse" />
+</div>
+<div class="studentList">
+	{#if originalStudents.length === 0}
+		<br />
+		Fant ingen elever i klassen 🤷‍♂️
+	{:else if students.length === 0}
+		<br />	
+		Fant ingen elever i klassen med søket 🤷‍♂️
+	{:else}
+	<div class="studentRow header">
+		<div class="studentInfo">Navn</div>
+		<div class="studentAction">Handling</div>
+	</div>
+		{#each students.slice(currentPage * studentsPerPage, (currentPage * studentsPerPage) + studentsPerPage) as student}
+			<div class=studentRow>
+				<div class="studentInfo">
+					{#if student.kontaktlarer}
+						<div class="contactTeacher" title="Du er kontaktlærer for denne eleven"><strong>Kontaktlærer</strong></div>
+					{/if}
+					<div class="studentName">
+						<a href="/elever/{student.feidenavnPrefix}">{student.navn}</a>
+					</div>
+					<div class="studentId">{student.feidenavnPrefix}</div>
+				</div>
+				<div class="studentAction" use:clickOutside on:click_outside={() => {studentMenus[student.elevnummer] = false}}>
+					<button class="action studentButton{studentMenus[student.elevnummer] ? ' cheatActive' : ''}" on:click={() => {studentMenus[student.elevnummer] = !studentMenus[student.elevnummer]}}>
+						<span class="material-symbols-outlined">more_vert</span>
+					</button>
+					{#if studentMenus[student.elevnummer]}
+						<div class="studentMenu">
+							<button class="blank studentMenuOption inward-focus-within" on:click={() => {goto(`/elever/${student.feidenavnPrefix}/nyttdokument`)}}>Nytt dokument</button>
+							<button class="blank studentMenuOption inward-focus-within" on:click={() => {goto(`/elever/${student.feidenavnPrefix}/nyttdokument?type=notat`)}}>Nytt notat</button>
+						</div>
+					{/if}
+				</div>
+			</div>
+		{/each}
+		<Pagination {currentPage} elementName={'elever'} elementsPerPage={studentsPerPage} maxPageNumbers={11} {gotoPage} {nextPage} {previousPage} numberOfElements={students.length} />
+	{/if}
+</div>
 
-<h3>Dette er en klasse</h3>
-<p>Klasseid: {$page.params.klasseid}</p>
+<style>
+	.studentRow {
+		display: flex;
+		align-items: center;
+		padding: 1rem 2rem;
+		gap: 0.5rem;
+	}
+	.studentRow.header {
+		padding: 1rem 2rem 0rem 2rem;
+	}
+	.studentInfo {
+		max-width: 11rem;
+		flex-grow: 1;
+	}
+	.contactTeacher {
+		font-size: var(--font-size-extra-small);
+	}
+	.studentId {
+		font-size: var(--font-size-small);
+	}
+	.classGroup {
+		font-size: var(--font-size-small);
+	}
+	.studentAction {
+		position: relative;
+		margin-left: auto;
+	}
+	.studentRow:nth-child(even) {
+		background-color: var(--primary-color-10);
+	}
+  .cheatActive {
+    background-color: rgba(0,0,0,0.1);
+  }
+
+  .studentMenu {
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+		min-width: 8rem;
+    right: 0.125rem;
+    top: 3rem;
+    border: 2px solid var(--primary-color);
+		z-index: 2; /* ? */
+  }
+  .studentMenuOption {
+    flex-grow: 1;
+    padding: 1rem;
+    background-color: var(--primary-background-color);
+  }
+  .studentMenuOption:hover {
+    padding: 1rem;
+    background-color: var(--primary-color-10);
+  }
+</style>
