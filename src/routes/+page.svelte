8adfad68021ae13c5f7a1cff1e505367bbe2a346
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2024-08-27 10:41:31 +0200

    some help info

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -38,6 +38,9 @@
     <div class="textBox">
         Her kan du opprette varsler, notater, eller dokumentere elevsamtaler.
     </div>
+    <div class="textBox">
+        Tilgang til elever og klasser i MinElev styres fra Visma InSchool. Ta kontakt med Visma InSchool ansvarlig på skolen din, så hjelper de deg!
+    </div>
     <br />
     {#if data.systemInfo.YFF_ENABLED}
         <h3 class="boxTitle">Om YFF-modulen</h3>
