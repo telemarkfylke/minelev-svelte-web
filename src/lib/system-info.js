930049958c2d7f9ee525aae47f526cd6046257bf
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2025-08-10 14:00:58 +0200

    feat: enhance document creation conditions and update UI guidance for teachers

--- src/lib/system-info.js
+++ src/lib/system-info.js
@@ -9,6 +9,7 @@ import { version } from '../../package.json'
  * @property {boolean} ELEVSAMTALE_READONLY
  * @property {boolean} NOTAT_READONLY
  * @property {boolean} YFF_READONLY
+ * @property {boolean} createDocumentAvailable
  *
  */
 
@@ -17,7 +18,7 @@ import { version } from '../../package.json'
  * @returns {SystemInfo}
  */
 export const getSystemInfo = () => {
-  return {
+  const systemInfo = {
     version,
     YFF_ENABLED: env.YFF_ENABLED === 'true',
     VARSEL_READONLY: env.VARSEL_READONLY === 'true',
@@ -25,4 +26,6 @@ export const getSystemInfo = () => {
     NOTAT_READONLY: env.NOTAT_READONLY === 'true',
     YFF_READONLY: env.YFF_READONLY === 'true'
   }
+  systemInfo.createDocumentAvailable = !systemInfo.VARSEL_READONLY || !systemInfo.ELEVSAMTALE_READONLY || (!systemInfo.YFF_READONLY && systemInfo.YFF_ENABLED)
+  return systemInfo
 }
